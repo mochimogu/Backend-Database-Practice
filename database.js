84382b3eeec37fcc44c0a7b9f66f271cd63c5297
thao0896 <thao0896@umn.edu>
2024-08-02 14:53:31 -0500

    finished all functionality except update

--- database.js
+++ database.js
@@ -1,28 +1,103 @@
-const pg = require('pg');
+const pg = require('pg').Pool;
 const { Client } = pg;
 
-const client = new Client({
-    user: 'maindatabase_ae1h_user',
-    password : 'zXQ4qe9TS0hnIewkVjaYdk0R0BTB0uA4',
-    host: "localhost",
-    port: 5432,
-    database: 'maindatabase_ae1h'
+const pool = new pg({
+    connectionString : "postgresql://maindatabase_ae1h_user:zXQ4qe9TS0hnIewkVjaYdk0R0BTB0uA4@dpg-cqlubkd6l47c73c1mb70-a.oregon-postgres.render.com/maindatabase_ae1h",
+    ssl : {
+        rejectUnauthorized : false
+    }
 })
 
 async function createTable() {
 
-    await client.connect();
+    try {
+        const client = await pool.connect();
 
-    const results = await client.query('CREATE TABLE hmong ( lastname VARCHAR(50))');
+        const results = await client.query('CREATE TABLE hmong ( lastname VARCHAR(50))');
+    
+        // console.log(results);
+    
+        await client.release();
+    
+        return 0;
+    } catch (error) {
+        
+        console.log(error);
 
-    console.log(results);
+    }
+}
 
-    await client.end();
+async function getAll() {
 
-    return 0;
+    try {
+        const client = await pool.connect();
+
+        const results = await client.query("SELECT * FROM hmong");
+    
+        console.log(results.rows);
+    
+        await client.release();
+    
+        return results.rows; 
+
+    } catch (error) {
+
+        console.log(error);
+
+    }
 }
 
 
+async function add(sending) {
 
-module.exports = { pg, createTable }
+    try {
+        // console.log(sending);
+
+        const cilent = await pool.connect();
+
+        const text = 'INSERT INTO hmong(lastname) VALUES ($1)';
+        const values = [sending.word]
+
+        await cilent.query(text, values);
+
+        await cilent.release();
+
+        return 0;
+        
+    } catch (error) {
+
+        console.log(error);
+        
+        return 1;
+    }
+
+}
+
+async function deleting(sending) {
+
+    try {
+        const cilent = await pool.connect();
+
+        const text = "DELETE FROM hmong WHERE lastname = $1";
+        const values = [sending.word];
+
+        const results = await cilent.query(text, values);
+        console.log(results.rows);
+        
+        await cilent.release();
+
+        return 0;
+
+    } catch (error) {
+        
+        console.log(error);
+        
+        return 1;
+
+    }
+
+
+}
+
+module.exports = { pg, createTable, getAll, add, deleting }
 
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const server = express();
 const PORT = 4321;
 
@@ -6,18 +7,64 @@ const db = require('./database.js')
 
 
 server.use(express.json());
+server.use(express.static(path.join(__dirname, 'html')));
+server.use(express.urlencoded({extended : true}));
 
-server.get('/create', (request, response) => {
-    const results = db.createTable();
+server.get('/get', async (request, response) => {
+    // const results = db.createTable();
+    const select = await db.getAll();
+    
+    console.log(select);
 
-    response.status(200).json({'results' : results});
+    response.status(200).json({'results' : 'successful', 'hmongWord' : select});
 })
 
+server.get('/admin/', (request, response) => {
+
+    response.status(200).sendFile(path.join(__dirname, 'html', 'index.html'));
+
+})
+
+server.post('/add', async (request, response) => {
+
+    const form = request.body.hmongInput;
+    console.log(form);
+
+    const sending = { word : form};
+
+    const results = await db.add(sending);
+
+    if(results === 0) {
+        response.status(200).json({'result' : 'successful'});
+    } else {
+        response.status(400).json({'result' : 'error'})
+    }
+
+})
+
+server.post('/delete', async (request, response) => {
+
+    const form = request.body.hmongDeleteInput;
+
+    const sending = { word : form};
+
+    const results = await db.deleting(sending);
+
+
+    if(results === 0) {
+        console.log('successful remove');
+        console.log(results);
+        response.status(200).json({'result' : 'successful'});
+    } else {
+        console.log('unsuccessful remove');
+        response.status(400).json({'result' : 'error'})
+    }
+
+})
 
 server.get('/', (request, response) => {
     response.status(200).json({'results' : 'successful'});
 })
-
 
 
 
